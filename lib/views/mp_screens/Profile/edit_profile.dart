764080d6d8d6a3ee8fe900ffe5308b838c1b3cc9
Waas9963 <wasananarasinghe63@gmail.com>
2021-01-13 20:01:43 +0530

    created edit profile interface

---
+++ lib/views/mp_screens/Profile/edit_profile.dart
@@ -0,0 +1,181 @@
+import 'dart:io';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:tyrex/views/Auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:tyrex/views/Auth/widgets/button.dart';
+import 'package:tyrex/views/Auth/widgets/textfield.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+
+class EditProfile extends StatefulWidget {
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  final _name = TextEditingController();
+  final _address = TextEditingController();
+  final _phone = TextEditingController();
+  final _bikename = TextEditingController();
+  final _model = TextEditingController();
+  final _regNo = TextEditingController();
+  final _password = TextEditingController();
+
+  String _errorTxt = '';
+  String _errorEmail = '';
+  String _errorPhone = '';
+  ProgressDialog pr;
+
+  @override
+  void dispose() {
+    _phone.dispose();
+    _errorTxt = "";
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenheight = MediaQuery.of(context).size.height;
+    final screenwidth = MediaQuery.of(context).size.width;
+    pr = new ProgressDialog(context, type: ProgressDialogType.Normal);
+
+    pr.style(
+        message: 'Verifying Phone...',
+        borderRadius: 10.0,
+        progressWidget: Container(
+          height: 30,
+          width: 30,
+          child: CircularProgressIndicator(),
+        ),
+        elevation: 10.0,
+        insetAnimCurve: Curves.easeInOut,
+        progressTextStyle: TextStyle(fontFamily: 'Montserrat'));
+
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      resizeToAvoidBottomPadding: false, // this avoids the overflow error
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+          child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Container(
+          // color: Colors.black,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          margin: EdgeInsets.only(top: screenheight / 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              // SizedBox(height: screenheight / 50),
+               Image.asset(
+                        'assets/images/profile.png',
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: MediaQuery.of(context).size.height / 9,
+                      ),
+                      SizedBox(height: screenheight / 40),
+              CustomTextFieldTwo(
+                controller: _name,
+                hintTxt: 'Full name',
+                isPassword: true,
+              ),
+              SizedBox(height: screenheight / 50),
+              CustomTextFieldTwo(
+                controller: _address,
+                hintTxt: 'Address',
+                isPassword: true,
+              ),
+              SizedBox(height: screenheight / 50),
+               CustomTextFieldTwo(
+                controller: _phone,
+                hintTxt: 'Mobile',
+                isPassword: true,
+              ),
+              SizedBox(height: screenheight / 50),
+               CustomTextFieldTwo(
+                controller: _bikename,
+                hintTxt: 'Bike Name',
+                isPassword: true,
+              ),
+              SizedBox(height: screenheight / 50),
+               CustomTextFieldTwo(
+                controller: _model,
+                hintTxt: 'Bike Model',
+                isPassword: true,
+              ),
+              SizedBox(height: screenheight / 50),
+               CustomTextFieldTwo(
+                controller: _regNo,
+                hintTxt: 'Register No',
+                isPassword: true,
+              ),
+              SizedBox(height: screenheight / 50),
+               CustomTextFieldTwo(
+                controller: _password,
+                hintTxt: 'Password',
+                isPassword: true,
+              ),
+              SizedBox(height: screenheight / 30),
+            
+              GestureDetector(
+                onTap: () {
+                  if (validate()) {
+                    
+                  }
+                },
+                child: Button(
+                  text: "Update Profile",
+                  color: Colors.black,
+                  btnTxtColor: Colors.white,
+                  height: screenheight / 15,
+                  width: screenwidth / 9,
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+
+  Future<dynamic> errorDialog(String title, String dec) {
+    return AwesomeDialog(
+            context: context,
+            dialogType: DialogType.ERROR,
+            animType: AnimType.TOPSLIDE,
+            tittle: title,
+            desc: dec,
+            // btnCancelOnPress: () {},
+            btnOkOnPress: () {})
+        .show();
+  }
+
+  bool validate() {
+    if (_phone.text.isEmpty &&
+        _address.text.isEmpty &&
+        _name.text.isEmpty &&
+        _bikename.text.isEmpty &&
+        _model.text.isEmpty &&
+        _regNo.text.isEmpty &&
+        _password.text.isEmpty) {
+      errorDialog('ERROR', 'Fields cant be empty !');
+
+      return false;
+    } else if (_phone.text.isEmpty ||
+        _address.text.isEmpty ||
+        _name.text.isEmpty ||
+        _bikename.text.isEmpty ||
+        _model.text.isEmpty ||
+        _regNo.text.isEmpty ||
+        _password.text.isEmpty) {
+      errorDialog('ERROR', 'Fields cant be empty !');
+      return false;
+    } else if (_phone.text.length <= 9) {
+      errorDialog('ERROR', 'phone no should long 10 digits !');
+
+      return false;
+    }
+   else{
+
+     return true;
+   }
+  }
+}
