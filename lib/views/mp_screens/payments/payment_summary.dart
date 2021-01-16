2c26b1b1e97433901e9cc54e32ede45c474e9f48
sewwandiranasinghe <56424319+sewwandiranasinghe@users.noreply.github.com>
2021-01-16 11:46:39 +0530

    payment history completed

--- lib/views/mp_screens/payments/payment_summary.dart
+++ lib/views/mp_screens/payments/payment_summary.dart
@@ -4,10 +4,34 @@ import 'package:tyrex/views/Auth/widgets/button.dart';
 import 'package:tyrex/views/Auth/widgets/textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:progress_dialog/progress_dialog.dart';
+import 'package:tyrex/views/mp_screens/home_screen/home_screen.dart';
+import 'package:tyrex/views/mp_screens/payments/card_payment_screen.dart';
+import 'package:tyrex/srevices/database.dart';
+import 'package:provider/provider.dart';
+import 'package:tyrex/models/user.dart';
 
 class PaymentSummary extends StatefulWidget {
   final double amount;
-  PaymentSummary({Key key, this.amount}) : super(key: key);
+  final String servicetype;
+  bool oilchange;
+  bool filters;
+  bool normalwash;
+  bool repairdamage;
+  bool highwash;
+  bool freepickup;
+  bool freedrop;
+  PaymentSummary(
+      {Key key,
+      this.amount,
+      this.servicetype,
+      this.oilchange,
+      this.filters,
+      this.normalwash,
+      this.repairdamage,
+      this.highwash,
+      this.freepickup,
+      this.freedrop})
+      : super(key: key);
   @override
   _PaymentSummaryState createState() => _PaymentSummaryState();
 }
@@ -23,7 +47,9 @@ class _PaymentSummaryState extends State<PaymentSummary> {
   void initState() {
     _amount.text = widget.amount.toString();
     _serviceTax.text = "200";
-    _totalAmount.text = (widget.amount + 200).toString(); 
+    _totalAmount.text = (widget.amount + 200).toString();
+    print(widget.servicetype);
+
     super.initState();
   }
 
@@ -36,12 +62,14 @@ class _PaymentSummaryState extends State<PaymentSummary> {
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+
     final screenheight = MediaQuery.of(context).size.height;
     final screenwidth = MediaQuery.of(context).size.width;
     pr = new ProgressDialog(context, type: ProgressDialogType.Normal);
 
     pr.style(
-        message: 'Verifying Phone...',
+        message: 'saving info',
         borderRadius: 10.0,
         progressWidget: Container(
           height: 30,
@@ -105,7 +133,20 @@ class _PaymentSummaryState extends State<PaymentSummary> {
                     SizedBox(height: screenheight / 30),
                     GestureDetector(
                       onTap: () {
-                        if (validate()) {}
+                        if (validate()) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => CardPaymentScreen(
+                                  amount: double.parse(_totalAmount.text),
+                                  servicetype: widget.servicetype,
+                                  oilchange: widget.oilchange,
+                                  filters: widget.filters,
+                                  normalwash: widget.normalwash,
+                                  repairdamage: widget.repairdamage,
+                                  highwash: widget.highwash,
+                                  freepickup: widget.freepickup,
+                                  freedrop: widget.freedrop,
+                                  )));
+                        }
                       },
                       child: Button(
                         text: "Credit/Debit Card Payment",
@@ -117,8 +158,25 @@ class _PaymentSummaryState extends State<PaymentSummary> {
                     ),
                     SizedBox(height: screenheight / 30),
                     GestureDetector(
-                      onTap: () {
-                        if (validate()) {}
+                      onTap: () async {
+                        if (validate()) {
+                          await DatabaseService(uid: user.uid)
+                              .saveBooking(
+                                  widget.servicetype,
+                                  widget.oilchange,
+                                  widget.filters,
+                                  widget.normalwash,
+                                  widget.repairdamage,
+                                  widget.highwash,
+                                  widget.freepickup,
+                                  widget.freedrop,
+                                  widget.amount,
+                                  'cash')
+                              .whenComplete(() {
+                            successDialog('SUCCESS',
+                                'Successfully booked your service !');
+                          });
+                        }
                       },
                       child: Button(
                         text: "Cash on pickup",
@@ -140,16 +198,18 @@ class _PaymentSummaryState extends State<PaymentSummary> {
 
   Future<dynamic> successDialog(String title, String dec) {
     return AwesomeDialog(
-            context: context,
-            dialogType: DialogType.SUCCES,
-            animType: AnimType.TOPSLIDE,
-            tittle: title,
-            desc: dec,
-            btnOkText: 'Ok',
-            btnCancelText: 'Cancel',
-            btnCancelOnPress: () {},
-            btnOkOnPress: () {})
-        .show();
+        context: context,
+        dialogType: DialogType.SUCCES,
+        animType: AnimType.TOPSLIDE,
+        tittle: title,
+        desc: dec,
+        btnOkText: 'Ok',
+        btnCancelText: 'Cancel',
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+        }).show();
   }
 
   Future<dynamic> errorDialog(String title, String dec) {
